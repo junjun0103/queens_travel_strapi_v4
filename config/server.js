fbd1979ec6f07f6edadb80496cb67d60c27c1bcf
Jun <email2mrjunlee@gmail.com>
2022-01-30 00:43:12 +1300

    test2

--- config/server.js
+++ config/server.js
@@ -1,9 +1,6 @@
-// module.exports = ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-// });
 module.exports = ({ env }) => ({
-  // ...,
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   graphql: {
     config: {
       endpoint: "/graphql",
