c4834b98f23de04b86376b9cd2a40798955ec19c
Jun <email2mrjunlee@gmail.com>
2022-01-30 00:50:47 +1300

    test3

--- config/server.js
+++ config/server.js
@@ -1,12 +1,4 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  graphql: {
-    config: {
-      endpoint: "/graphql",
-      apolloServer: {
-        introspection: true,
-      },
-    },
-  },
 });
